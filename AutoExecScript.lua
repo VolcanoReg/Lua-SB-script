1aba0d5a259f14a2e04100984afe5c86a2803434
VolcanoReg <90879632+VolcanoReg@users.noreply.github.com>
2023-07-05 03:39:22 +0700

    Update AutoExecScript.lua

--- AutoExecScript.lua
+++ AutoExecScript.lua
@@ -4,36 +4,47 @@ local Scripts = {
 local Running = {}
 local TIMER = 10
 local Http = game:GetService("HttpService")
-local CBA = Instance.new("RemoteEvent")
 script.Parent = owner
-CBA.Name = "CommandBasedAction"
-CBA.Parent = owner
+
+for i,v in pairs(owner:GetChildren()) do
+	if v.Name == "CommandBasedAction" then
+		v:Destroy()
+	end
+end
+
+CommandAction = Instance.new("RemoteEvent")
+CommandAction.Name = "CommandBasedAction"
+CommandAction.Parent = owner
 
 --NLS
+Client = function()
 NLS(
 [[
 local remote = owner:WaitForChild("CommandBasedAction")
+print(remote)
 local chat = game:GetService("Chat")
-prefix = &
+prefix = "&"
 chat.Chatted:Connect(function(part,msg,color)
     if part.Name == game.Players.LocalPlayer.Name and string.sub(msg,1,1) == prefix then
         remote:FireServer(string.sub(msg,2))
     end
 end)
 remote.OnClientEvent:Connect(function(msg)
-	game:GetService("Chat"):Chat(owner.Character,msg)
+	print(msg)
+	chat:Chat(owner.Character,msg)
 end)
-]],script)
-
+]])
+end
+Client()
 --Http function used to get the script
-function ScriptHttpChecker(url,nocache,header)
+function ScriptHttpChecker(url,nocache)
 	checker = 0
 	MAX_RETRY = 5
 	local noerror,code
 	repeat 
 		checker += 1
 		print("Try "..checker)
-		noerror,code = pcall(Http:GetAsync,url,nocache,header or {})
+		noerror,code = pcall(function(url,nocache) return Http:GetAsync(url,nocache) end,url,nocache)
 		task.wait(1)
 	until noerror == true or checker == MAX_RETRY
 	return noerror,code
@@ -42,13 +53,14 @@ end
 functions_list = {
 	{"Init",function(name)
 		for i,scripta in next,Scripts do
-			if name == script[2] then
+			if name == scripta[2] then
 				print("Auto Executing "..scripta[2])
 				noerror,codes = ScriptHttpChecker(scripta[1],true)
 				if noerror == false then
+					CommandAction:FireAllClients("No Running Scripts")
 					print("Error getting script")
 				else
-					table.insert(Running,{script[2],NS(codes),codes})
+					table.insert(Running,{scripta[2],NS(codes),codes})
 				end
 			end
 		end
@@ -58,7 +70,7 @@ functions_list = {
 	end
 	},
 }
-CBA.OnServerEvent:Connect(function(_,msg)
+CommandAction.OnServerEvent:Connect(function(_,msg)
 	msg = string.split(msg," ")
 	for i,commands in next,functions_list do
 		if msg[1] == commands[1] then
@@ -70,25 +82,31 @@ end)
 --The Scripts Autochecker and Autoexecutor
 while true do
 	if #Running < 1 then
-		CBA:FireAllClient("No Running Scripts")
+		CommandAction:FireAllClients("No Running Scripts")
 		print("No Running Scripts")
 	else
 		for i,scripta in next,Running do
 			newcode = nil
 			for i,code in next,Scripts do
 				if scripta[1] == code[2] then
-					newcode = ScriptHttpChecker(code[1],true)
+					noerror,newcode = ScriptHttpChecker(code[1],true)
+					if noerror == false then
+						CommandAction:FireAllClients("No Running Scripts")
+						print("Error getting script")
+					end
+				else
 				end
 			end
 			if scripta[3] ~= newcode then
 				print("New Code Detected")
-				CBA:FireAllClient("New Code Detected")
+				CommandAction:FireAllClients("New Code Detected")
 				owner:LoadCharacter()
+				Client()
 				scripta[2]:Destroy() --destroy old NS
 				scripta[2] = NS(newcode) --replace with the new ones
 				scripta[3] = newcode -- replace old code with new code
 			end
-			CBA:FireAllClient("No New Code Detected")
+			CommandAction:FireAllClients("No New Code Detected")
 			print("No New Code Detected")
 		end
 	end
